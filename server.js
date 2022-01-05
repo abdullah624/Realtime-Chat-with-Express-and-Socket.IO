aaa3095765409126de09f47176f95a4ad463d1e4
Mokbul Hossain <abdullahalmokbul@gmail.com>
2022-01-05 16:41:17 +0600

    Create a simple express server

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+
+const PORT = 3000;
+
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
