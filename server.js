7ee24087802739144763527222d7e81103558f6d
Mokbul Hossain <abdullahalmokbul@gmail.com>
2022-01-05 16:52:02 +0600

    Integrate socket io server over express app

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 const PORT = 3000;
 
